b56525316a8e410df78cc72ca4072c96ba8d5acc
An0n-00 <142223573+An0n-00@users.noreply.github.com>
2024-06-13 20:34:59 +0200

    sticky header on scroll

--- web/scripts/main.js
+++ web/scripts/main.js
@@ -1,3 +1,4 @@
+// Menu Click
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.body.style.backgroundColor = "#A5A5A5FF";
@@ -10,4 +11,19 @@ function closeNav() {
     document.body.style.backgroundColor = "white";
     document.querySelector('#searchbar').style.backgroundColor = "#fff";
     document.querySelector('#searchbutton').style.color = "#000";
+}
+
+// Make the header sticky on scroll
+window.onscroll = function() {onScroll()};
+
+var header = document.getElementById("stickyhead");
+
+var sticky = header.offsetTop;
+
+function onScroll() {
+    if (window.scrollY > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
 }
