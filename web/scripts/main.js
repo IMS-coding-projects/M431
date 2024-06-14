839dee6a4c8f1cfe8e190766d772a5d4aec55663
An0n-00 <142223573+An0n-00@users.noreply.github.com>
2024-06-14 14:35:19 +0200

    main content has been styled

--- web/scripts/main.js
+++ web/scripts/main.js
@@ -46,3 +46,20 @@ if (!q && isSearch) {
 } else {
 document.getElementById('search-query').textContent = "Showing results for " + q;
 }
+
+// Block ads
+const clear = (() => {
+    const defined = v => v !== null && v !== undefined;
+    const timeout = setInterval(() => {
+        const ad = [...document.querySelectorAll('.ad-showing')][0];
+        if (defined(ad)) {
+            const video = document.querySelector('video');
+            if (defined(video)) {
+                video.currentTime = video.duration;
+            }
+        }
+    }, 500);
+    return function() {
+        clearTimeout(timeout);
+    }
+})();
